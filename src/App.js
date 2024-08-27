3335b441b756315386f1f9bb0ebb77b4ae06d237
Mark McCarthy <mm3guides@gmail.com>
2024-08-27 16:00:08 +0100

    register user added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ function App() {
   // Normal JavaScript goes here
   const [toggle,setToggle] = useState(true);
   const [imageArray, setImageArray] = useState([]);
+  const [userid,setUserid] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("")
+
+  //SUBMITHANDLER
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    console.log(userid,email, password);
+    const response = await fetch(
+      "http://localhost:5001/users/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+          username: userid,
+          email:email,
+          password: password
+        })
+      }
+    );
+    const output = await response.json();
+    console.log(response);
+    console.log(output);
+  }
 
   //FETCH
   const getImages = async () => {
@@ -15,6 +41,17 @@ function App() {
   useEffect(() => {getImages()},[])
   return (
     <div className="App">
+    <div>
+      <form onSubmit={submitHandler}>
+        <label>User Id</label><br></br>
+        <input type='text'name='userid' onChange={(event) =>{setUserid(event.target.value)} }/><br></br><br></br>
+        <label>Email</label><br></br>
+        <input type='text'name='email' onChange={(event) =>{setEmail(event.target.value)}}/><br></br><br></br>
+        <label>Password</label><br></br>
+        <input type='text'name='password' onChange={(event) =>{setPassword(event.target.value)}}/><br></br><br></br>
+        <input type='submit' value="Submit"></input>
+      </form>
+    </div>
       <h1>List</h1>
       <button onClick={(event) => {setToggle(!toggle);console.log(toggle)}}>Press me</button>
       <br></br>
