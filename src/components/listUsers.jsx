fe8b3444b14ab80427df489d764261191ab40c49
Mark McCarthy <mm3guides@gmail.com>
2024-08-28 13:59:17 +0100

    list users working@

--- src/components/listUsers.jsx
+++ src/components/listUsers.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import readcookie from '../utils/readcookie';
 
 const ListUsers = () => {
-
+    const [userList,setUserList]= useState([])
+    const [listOn,setListOn] = useState(false)
     //CLICKHANDLER
+
+    const clickHandler2 = async (event) => {
+        setListOn(false);
+    }
   const clickHandler = async (event) => {
     let token = readcookie("jwt_token");
     console.log(token);
@@ -19,8 +25,9 @@ const ListUsers = () => {
       }
     );
     const output = await response.json();
-    console.log(response);
-    console.log(output);
+    console.log(output.userlist);
+    setUserList(output.userlist);
+    setListOn(true);
     }
 
     return(
@@ -28,6 +35,21 @@ const ListUsers = () => {
     <hr></hr>
     <h1>List Users</h1>
     <button onClick={clickHandler}>List Users</button>
+    <button onClick={clickHandler2}>Switch off List</button>
+    <hr></hr>
+    <div>
+        <h1>List of Users</h1>
+        {userList.map((item, index) => {
+            return(
+            <>
+            {listOn && <>
+            <h2>User ID = {item.username}</h2>
+            <h2>Email = {item.email}</h2>
+            </>
+        }
+            </>)
+        })}
+    </div>
     <hr></hr>
   </div>
 )}
--- src/utils/readcookie.js
+++ src/utils/readcookie.js
@@ -2,7 +2,6 @@ function readcookie(key) {
     const cookieRegEx = new RegExp(`(?<=${key}=)[^;]*`);
     try {
         let token = document.cookie.match(cookieRegEx)[0];
-        console.log("readcookie>>",token);
         return token;
     } catch (error) {
         console.log(error);
